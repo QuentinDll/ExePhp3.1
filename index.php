2f71a2d17f9dbdafeebc2b069d86d7f41a16e975
QuentinDll <quentin.delplace13@gmail.com>
2018-07-26 16:19:53 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Exercice 3</title>
+</head>
+<body>
+  <p>
+    <?php
+    $km = 1;
+    echo $km;
+    echo $km += 2;
+    echo $km += 122;
+     ?>
+  </p>
+</body>
+</html>
